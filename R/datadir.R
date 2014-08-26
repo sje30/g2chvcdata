12a4e8b56e024c8d5df9885bf72bfb66e0b45698
Stephen J. Eglen <sje30@cam.ac.uk>
2014-08-26 13:57:16 +0100

    @export this function.

--- R/datadir.R
+++ R/datadir.R
@@ -1,5 +1,12 @@
+##' Point to where the G2CHVC data files are stored.
+##'
+##' HDF5 files are bundled into this package.  Aside from this simple
+##' function below, there is no code in this package.
+##' @title Return directory where HDF5 data is stored for the g2chvc project.
+##' @return The pathname as a string.
+##' @author Stephen Eglen
+##' @export
 g2chvcdatadir <- function() {
-  ## Return directory containing hdf5 files.
   system.file("extdata/", package="g2chvcdata")
 }
 
